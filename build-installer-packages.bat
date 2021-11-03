1ee37c4a5702c215edb49b8ff9a90f433528a884
Aaron Carpenter <aaronc15@uw.edu>
2021-11-03 13:37:11 -0700

    updating icon path to be platform agnostic, deleting window batch build files

--- build-installer-packages.bat
+++
@@ -1,7 +0,0 @@
-cd c:\users\amcar\source\repos\fork-board	
-
-del .\dist\fork-board-win32-x64\resources\app\resources\config\wallets.json
-del .\dist\fork-board-win32-x64\resources\app\resources\config\clientconfig.json
-
-electron-installer-windows --src dist\fork-board-win32-x64\ --dest dist\installers\
-
--- build-packages.bat
+++
@@ -1,2 +0,0 @@
-cd c:\users\amcar\source\repos\fork-board
-electron-packager . fork-board --all --out dist\ --overwrite
